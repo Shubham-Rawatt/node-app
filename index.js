6a00fae9d58e0f403bd1901e30673ad79f63a3c9
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-11-22 21:51:47 +0530

    create routes in server

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // const http = require('http') 
 import http from 'http'
 
-const server = http.createServer((req , res) =>{
-    res.end("this is my first server")
+const server = http.createServer((req , res) =>{ 
+    if(req.url == "/"){
+        res.end('welcome to home route')
+    } else if(req.url == "/about"){
+        req.url('welcome to about')
+    } else if (req.url == "/contact"){
+        req.url('welcome to contact')
+    } else{
+        res.end('404 page not found')
+    }
 })
 
 server.listen(8000)
