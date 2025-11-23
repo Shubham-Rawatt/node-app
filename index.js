a2d13d617463acb1ce947aa2cafe875833522ba2
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-11-23 10:34:49 +0530

    create new server

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-// const http = require('http') 
-import http from 'http'
+// // const http = require('http')
+// import http from "http";
 
-const server = http.createServer((req , res) =>{ 
-    if(req.url == "/"){
-        res.end('welcome to home route')
-    } else if(req.url == "/about"){
-        req.url('welcome to about')
-    } else if (req.url == "/contact"){
-        req.url('welcome to contact')
-    } else{
-        res.end('404 page not found')
-    }
-})
+// const server = http.createServer((req, res) => {
+//   if (req.url == "/") {
+//     res.end("welcome to home");
+//   } else if (req.url == "/about") {
+//     req.url("welcome to about");
+//   } else if (req.url == "/contact") {
+//     req.url("welcome to contact");
+//   } else {
+//     res.end("404 page not found");
+//   }
+// });
 
-server.listen(8000)
+// server.listen(8000, () => {
+//   console.log("server is running");
+// });
+
+
+
+// const http = require('http')
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.write("server is running now");
+  res.end('server is end');
+});
+
+server.listen(8000, () => {
+  console.log("server is running on https://localhost:8000");
+});
